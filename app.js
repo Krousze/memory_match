5a0eac7a2f464d50ed2c328f2570b1fd8266c4cc
DavidMalloy <Krouszetalison@gmail.com>
2019-12-03 11:31:21 -0800

    added flip method and css info as well as a eventlistener

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-console.log(new Card(fileNames[0]));
-//creates an array of card objects for each file
-const createCards = () => fileNames.map(fileName => new Card(fileName));
-
-const cards = createCards();
-const body = document.getElementById('body');
-cards.forEach(card => card.render(body));
+Game.setUpGame();
