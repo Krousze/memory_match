97797c183cfe7574d228647f7e59160a1ecefc14
cgutierrez7 <chris.n.gutierrez@gmail.com>
2019-12-03 09:13:56 -0800

    finished card class

--- Card.js
+++ Card.js
@@ -1,9 +1,22 @@
 export class Card {
-    constructor(url) {
-        this.imageUrl = url;
-    }
     
-    render() {
-        
+    constructor(fileName) {
+        this.fileName = fileName;
     }
+
+    //method renders individual card image
+    render(parent) {
+        //creates <img>
+        const img = document.createElement('img');
+        //adds src attribute <img src='img/fileName.jpg'>
+        img.src = this.getUrl();
+        //adds alt attribute <img src='img/fileName.jpg' alt='fileName'>
+        img.alt = this.getAltText();
+        //adds <img src='img/fileName.jpg' alt='fileName'> to DOM
+        parent.appendChild(img);
+    }
+
+    getUrl = () => 'img/' + this.fileName;
+
+    getAltText = () => this.fileName.substring(0, this.fileName.lastIndexOf('.'));
 }
