705771f594ec5434c36dc1233e7ae522b44df91b
cgutierrez7 <chris.n.gutierrez@gmail.com>
2019-12-06 09:11:43 -0800

    added background and lose condition

--- js/modal.js
+++ js/modal.js
@@ -2,5 +2,7 @@ $('#quit').click();
 $('#restart').on('click',function(){
     Game.getRandomImage();
     Game.counter = 0;
+    Game.CARD_FLIP_COUNTER = 0;
+    Game.TURN_COUNT = 0;
     $('#myModal').modal('hide');
 });
